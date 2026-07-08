bf32f66e527cdd13397b838861e8aa09b4b66c22
KR655 <koushikr655@gmail.com>
2026-07-09 01:35:38 +0530

    Use environment variables for API URL

--- weather-frontend/src/api/api.js
+++ weather-frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+const API_URL = import.meta.env.VITE_API_URL;
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: `${API_URL}/api/`,
   headers: {
     "Content-Type": "application/json",
   },
@@ -32,7 +32,7 @@ api.interceptors.response.use(
     ) {
       try {
         const response = await axios.post(
-          "http://127.0.0.1:8000/api/refresh/",
+           `${API_URL}/api/refresh/`,
           {
             refresh: localStorage.getItem("refresh"),
           }
