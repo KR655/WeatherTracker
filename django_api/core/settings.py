cf6777dc4cc61c2127bd4522deb690b4a64642e4
KR655 <koushikr655@gmail.com>
2026-07-08 22:36:01 +0530

    Fix GitHub Actions Redis configuration

--- django_api/core/settings.py
+++ django_api/core/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 from datetime import timedelta
-
+import redis
 from pathlib import Path
 from decouple import config
 WEATHER_API_KEY = config("WEATHER_API_KEY")
--- django_api/tracker/tests/test_weather.py
+++ django_api/tracker/tests/test_weather.py
@@ -1,12 +1,36 @@
+from unittest.mock import patch
 from django.test import TestCase
 from tracker.services import WeatherService
 
-
 class WeatherServiceTest(TestCase):
 
-    def test_weather_fetch(self):
+    @patch("tracker.services.requests.get")
+    def test_weather_fetch(self, mock_get):
+
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = {
+            "location": {
+                "name": "Chennai",
+                "country": "India",
+                "region": "Tamil Nadu",
+                "localtime": "2026-07-08 20:00",
+                "lat": 13.08,
+                "lon": 80.27
+            },
+            "current": {
+                "temp_c": 30,
+                "feelslike_c": 34,
+                "humidity": 80,
+                "wind_kph": 18,
+                "pressure_mb": 1008,
+                "uv": 5,
+                "condition": {
+                    "text": "Sunny",
+                    "icon": "//cdn.weatherapi.com/icon.png"
+                }
+            }
+        }
 
         weather = WeatherService.fetch_weather("Chennai")
 
-        self.assertIsNotNone(weather)
         self.assertEqual(weather["city"], "Chennai")
